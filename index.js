9ee46c9e2a095eb55ba1cca6123d6568ce98955d
IlimaG <ivanilimag@gmail.com>
2023-02-22 19:46:16 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+const users = require("./users.json");
+
+app.get("/", (request, response) => {
+    response.send({response: true, code: 200, users: users});
+});
+
+app.get("/:id", (request, response) => {
+    const { id } = request.params;
+    const results = users.filter(user => user.id === Number(id));
+    response.status(200).send({response: true, users: results});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
